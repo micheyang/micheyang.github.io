3335a5be7dca0ed6cc355d526421bf5f86511de9
micheyang <michelleandschool@gmail.com>
2020-11-16 17:17:10 -0500

    Adding deployment changes

---
+++ build/precache-manifest.1b989c5bf88154acfb095bb3f9f92637.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf5a7b28cae347ad26d98bfb86dfe0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae2527613bcb175b9c73",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "031d501262ba9ddde7e3",
+    "url": "/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "8c3b988675b31bf36a34",
+    "url": "/static/css/main.256e37ad.chunk.css"
+  },
+  {
+    "revision": "c85291d0fc50bf80b575",
+    "url": "/static/js/2.f2633c97.chunk.js"
+  },
+  {
+    "revision": "ae2527613bcb175b9c73",
+    "url": "/static/js/3.c07e5805.chunk.js"
+  },
+  {
+    "revision": "031d501262ba9ddde7e3",
+    "url": "/static/js/4.eed55297.chunk.js"
+  },
+  {
+    "revision": "8c3b988675b31bf36a34",
+    "url": "/static/js/main.6160173a.chunk.js"
+  },
+  {
+    "revision": "62d828cf0320422a5e62",
+    "url": "/static/js/runtime-main.60aea316.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f18a6b0c6f7d7d75695ade1ff03dd09c",
+    "url": "/static/media/barclaysLogo.f18a6b0c.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "b01fff4446565e2f20453bd12ea1f9cf",
+    "url": "/static/media/gt-logo-gold.b01fff44.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b5453c320c1c34eb9367709b7b2a6941",
+    "url": "/static/media/ncrLogo.b5453c32.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "57e9fa805da79d933c724133426d06e0",
+    "url": "/static/media/nus_logo_full-horizontal.57e9fa80.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/static/media/talksCardBackAlt.7f0feeed.svg"
+  },
+  {
+    "revision": "667f4ab90e5115c54fff686c2b2176e9",
+    "url": "/static/media/upsLogo.667f4ab9.png"
+  }
+]);
